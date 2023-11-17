a802371ba4182f89aca3c30ca09a8e2a685fc0ea
Guirn13 <111617011+Guirn13@users.noreply.github.com>
2023-11-16 21:43:18 -0300

    Add files via upload

---
+++ Pratica7/src/pratica7/Main.java
@@ -0,0 +1,40 @@
+package pratica7;
+
+import javax.swing.*;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+
+        int menu= 0;
+
+        do {
+            menu = Integer.parseInt(JOptionPane.showInputDialog("1- adicionar | 2- remover"));
+
+            switch (menu) {
+                case 1:
+                    String id_isbn, nm_titulo;
+                    int id_categoria, id_editora;
+                    double vl_preco;
+
+                    id_isbn = JOptionPane.showInputDialog("Digite ISBN");
+                    nm_titulo = JOptionPane.showInputDialog("Digite titulo");
+                    id_categoria = Integer.parseInt(JOptionPane.showInputDialog("Digite codigo da categoria"));
+                    id_editora = Integer.parseInt(JOptionPane.showInputDialog("Digite codigo da editora"));
+                    vl_preco = Double.parseDouble(JOptionPane.showInputDialog("Digite preço do livro"));
+
+                    Inserir inserir = new Inserir(id_isbn, id_categoria, id_editora, nm_titulo, vl_preco);
+                    break;
+                case 2:
+                    String cod_excluir = JOptionPane.showInputDialog("Digite o ISBN");
+                    Delete delete = new Delete(cod_excluir);
+                    break;
+                case 3:
+
+                    break;
+            }
+
+        } while (menu != 5);
+    }
+}
